77066deb6925a037ac088dbe02a7fd6838f3775c
Dimitri Sabadie <dimitri.sabadie@gmail.com>
2019-09-24 01:36:58 +0200

    Update editor to move points around and Bézier curves.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 // This is currently a prototype. The current code is pretty naive, especially in terms of
 // allocation of keys in splines. Some  work must be done to clean all that stuff.
 
+use cgmath::Vector2;
 use luminance::blending::{Equation, Factor};
 use luminance::context::GraphicsContext;
 use luminance::depth_test::DepthTest;
@@ -35,7 +36,7 @@ const DELTA_T: f32 = 0.01;
 const POINT_SELECTION_DIST: f32 = DELTA_T * 8.;
 
 /// Rebuild tessellation based on control points for lines.
-fn build_lines<C>(ctx: &mut C, spline: &Spline<f32, f32>) -> Result<Tess, String> where C: GraphicsContext {
+fn build_lines<C>(ctx: &mut C, spline: &Spline<f32, Vector2<f32>>) -> Result<Tess, String> where C: GraphicsContext {
   let mut vertices = Vec::new();
   let keys = spline.keys();
 
@@ -44,7 +45,8 @@ fn build_lines<C>(ctx: &mut C, spline: &Spline<f32, f32>) -> Result<Tess, String
     let mut t = keys[0].t;
 
     while t <= up_t {
-      vertices.push(LineVertex::new(VPos::new([t, spline.clamped_sample(t).unwrap()])));
+      let p = spline.clamped_sample(t).unwrap();
+      vertices.push(LineVertex::new(VPos::new(p.into())));
       t += DELTA_T;
     }
   }
@@ -57,32 +59,61 @@ fn build_lines<C>(ctx: &mut C, spline: &Spline<f32, f32>) -> Result<Tess, String
 }
 
 /// Rebuild tessellation based on control points for points.
-fn build_points<C>(ctx: &mut C, spline: &Spline<f32, f32>, selected: Option<usize>) -> Result<Tess, String> where C: GraphicsContext {
+fn build_points<C>(ctx: &mut C, spline: &Spline<f32, Vector2<f32>>, selected: Option<Selection>) -> Result<Tess, String> where C: GraphicsContext {
   let mut vertices = Vec::new();
   let keys = spline.keys();
 
+  let mut specials = Vec::new();
+
   if !keys.is_empty() {
-    for cp in keys {
-      vertices.push(
-        PointVertex::new(
-          VPos::new([cp.t, cp.value]),
-          VColor::new([0.5, 0.5, 1.]),
-          VRadius::new(0.025 / 2.),
-        )
+    for (i, cp) in keys.iter().enumerate() {
+      let mut vertex = PointVertex::new(
+        VPos::new(cp.value.into()),
+        VColor::new([0.5, 0.5, 1.]),
+        VRadius::new(0.025 / 2.),
       );
+
+      if let Some(Selection::Point(i_sel)) = selected {
+        if i_sel == i {
+          vertex.1 = VColor::new([1., 0.5, 0.5]);
+          vertex.2 = VRadius::new(0.025 / 2.);
+        }
+      }
+
+      vertices.push(vertex);
+
+      if let Interpolation::Bezier(u) = cp.interpolation {
+        let mut vertex = PointVertex::new(
+          VPos::new(u.into()),
+          VColor::new([0.5, 1., 0.5]),
+          VRadius::new(0.015 / 2.)
+        );
+
+        if let Some(Selection::Handle(i_sel)) = selected {
+          if i_sel == i {
+            vertex.1 = VColor::new([1., 0.5, 0.5]);
+            vertex.2 = VRadius::new(0.015 / 2.);
+          }
+        }
+
+        specials.push(vertex);
+      }
     }
   }
 
-  if let Some(i) = selected {
-    vertices[i].1 = VColor::new([1., 0.5, 0.5]);
-    vertices[i].2 = VRadius::new(0.05 / 2.);
-  }
+  vertices.extend(specials);
 
   TessBuilder::new(ctx)
     .set_mode(Mode::Point)
     .add_vertices(vertices)
     .build()
     .map_err(|e| format!("{:?}", e))
+}
+
+#[derive(Clone, Copy, Debug)]
+enum Selection {
+  Point(usize),
+  Handle(usize)
 }
 
 const LINE_VS_SRC: &str = include_str!("vs.glsl");
@@ -99,7 +130,7 @@ fn main() {
     .expect("create surface");
 
   // the actual spline
-  let mut spline: Spline<f32, f32> = Spline::from_vec(Vec::new());
+  let mut spline = Spline::from_vec(Vec::new());
 
   // misc
   let mut cursor_pos: Option<[f32; 2]> = None;
@@ -116,6 +147,8 @@ fn main() {
     .expect("shader program")
     .ignore_warnings();
 
+  let mut mouse_left_pressed = false;
+
   'app: loop {
     let mut rebuild_tess = false;
 
@@ -129,53 +162,109 @@ fn main() {
         }
 
         WindowEvent::CursorPos(x, y) => {
-          cursor_pos = Some([x as f32 / WINDOW_WIDTH as f32, 1. - 2. * y as f32 / WINDOW_HEIGHT as f32]);
+          let xy = [x as f32 / WINDOW_WIDTH as f32, 1. - 2. * y as f32 / WINDOW_HEIGHT as f32];
+          cursor_pos = Some(xy);
           println!("cursor pos: ({}, {}) -> {:?}", x, y, cursor_pos);
+
+          if mouse_left_pressed {
+            if let Some(selection) = selected_point {
+              match selection {
+                Selection::Point(i) => {
+                  println!("moving point {} to ({}, {})", i, x, y);
+                  let key = spline.get_mut(i).unwrap();
+                  *key.value = Vector2::new(xy[0], xy[1]);
+                }
+
+                Selection::Handle(i) => {
+                  println!("moving tangent handle of point {} to ({}, {})", i, x, y);
+                  let key = spline.get_mut(i).unwrap();
+
+                  if let Interpolation::Bezier(ref mut handle) = *key.interpolation {
+                    *handle = Vector2::new(xy[0], xy[1]);
+                  }
+                }
+              }
+
+              rebuild_tess = true;
+            }
+          }
         }
 
         WindowEvent::MouseButton(MouseButton::Button1, Action::Release, _) => {
-          if let Some([x, y]) = cursor_pos {
-            println!("adding a point ({}, {})", x, y);
+          mouse_left_pressed = false;
 
-            let mut cps = spline.keys().to_owned();
-            cps.push(Key::new(x, y, Interpolation::Cosine));
-            spline = Spline::from_vec(cps);
+          if selected_point.is_none() {
+            if let Some([x, y]) = cursor_pos {
+              println!("adding a point ({}, {})", x, y);
 
-            selected_point = None;
-            rebuild_tess = true;
+              spline.add(Key::new(spline.len() as f32, Vector2::new(x, y), Interpolation::Cosine));
+
+              selected_point = None;
+              rebuild_tess = true;
+            }
           }
+        }
+
+        WindowEvent::MouseButton(MouseButton::Button1, Action::Press, _) => {
+          mouse_left_pressed = true;
         }
 
         WindowEvent::MouseButton(MouseButton::Button2, Action::Release, _) => {
           if let Some([x, y]) = cursor_pos {
+            let [x, y] = [x as f32, y as f32];
             let mut found = None;
 
             // we want to select a point; check if any is nearby
             for (i, p) in spline.keys().iter().enumerate() {
-              let [x, y] = [x as f32, y as f32];
-              let [px, py] = [p.t, p.value];
+              let [px, py]: [f32; 2] = p.value.into();
               let dist = ((x - px).powf(2.) + (y - py).powf(2.)).sqrt();
 
               if dist <= POINT_SELECTION_DIST {
                 match found {
                   Some((_, prev_dist)) if dist < prev_dist => {
-                    found = Some((i, dist));
+                    found = Some((Selection::Point(i), dist));
                   }
 
                   None => {
-                    found = Some((i, dist));
+                    found = Some((Selection::Point(i), dist));
                   }
 
                   _ => ()
                 }
+              } else if let Interpolation::Bezier(ref handle) = p.interpolation {
+                let [px, py]: [f32; 2] = (*handle).into();
+                let dist = ((x - px).powf(2.) + (y - py).powf(2.)).sqrt();
+
+                if dist <= POINT_SELECTION_DIST {
+                  match found {
+                    Some((_, prev_dist)) if dist < prev_dist => {
+                      found = Some((Selection::Handle(i), dist));
+                    }
+
+                    None => {
+                      found = Some((Selection::Handle(i), dist));
+                    }
+
+                    _ => ()
+                  }
+                }
               }
             }
 
-            if let Some((i, _)) = found {
-              println!("selecting point {}", i);
-              selected_point = Some(i);
-            } else {
-              selected_point = None;
+            match found {
+              Some((Selection::Point(i), _)) => {
+                println!("selecting point {}", i);
+                selected_point = Some(Selection::Point(i));
+              }
+
+              Some((Selection::Handle(i), _)) => {
+                println!("selecting handle {}", i);
+                selected_point = Some(Selection::Handle(i));
+              }
+
+              _ => {
+                selected_point = None;
+              }
             }
 
             rebuild_tess = true;
@@ -183,13 +272,26 @@ fn main() {
         }
 
         WindowEvent::Key(GKey::Backspace, _, Action::Release, _) => {
-          if let Some(i) = selected_point {
+          if let Some(Selection::Point(i)) = selected_point {
             let mut keys: Vec<_> = spline.keys().into_iter().cloned().collect();
             keys.swap_remove(i);
             spline = Spline::from_vec(keys);
 
             selected_point = None;
             rebuild_tess = true;
+          }
+        }
+
+        WindowEvent::Key(GKey::Space, _, Action::Release, _) => {
+          if let Some(Selection::Point(i)) = selected_point {
+            if let Some(key) = spline.get_mut(i) {
+              let prev = *key.interpolation;
+              *key.interpolation = toggle_interpolation(*key.value, prev);
+
+              println!("toggling interpolation for key {}; {:?} -> {:?}", i, prev, key.interpolation);
+
+              rebuild_tess = true;
+            }
           }
         }
 
@@ -225,5 +327,15 @@ fn main() {
     });
 
     surface.swap_buffers();
+  }
+}
+
+fn toggle_interpolation(p: Vector2<f32>, i: Interpolation<f32, Vector2<f32>>) -> Interpolation<f32, Vector2<f32>> {
+  match i {
+    Interpolation::Step(_) => Interpolation::Linear,
+    Interpolation::Linear => Interpolation::Cosine,
+    Interpolation::Cosine => Interpolation::Bezier(p + Vector2::new(0.1, 0.1)),
+    Interpolation::Bezier(_) => Interpolation::Step(0.5),
+    _ => i
   }
 }
